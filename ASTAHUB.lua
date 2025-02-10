a842f0dbb95d22c45cc82f3c7d2ab6015674b598
BloxScripter514 <abodatagrebe@gmail.com>
2025-02-10 19:54:45 +0200

    ASTAHUB.lua

--- ASTAHUB.lua
+++ ASTAHUB.lua
@@ -1,6 +1,14 @@
-local Window = Library.CreateLib("ASTA", "DarkTheme")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("ASTAHUB", "Midnight")
 local Tab = Window:NewTab("Tab")
 local Section = Tab:NewSection("Farm")
 Section:NewButton("Auto farm level", "ButtonInfo", function()
     print("Clicked")
+end)
+Section:NewToggle("ToggleText", "ToggleInfo", function(state)
+    if state then
+        print("Toggle On")
+    else
+        print("Toggle Off")
+    end
 end)
