0b640a905e7e2ce3795f9c985351c33718f64fb5
BloxScripter514 <abodatagrebe@gmail.com>
2025-02-10 20:14:09 +0200

    ASTAHUB.lua

--- ASTAHUB.lua
+++ ASTAHUB.lua
@@ -2,13 +2,6 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 local Window = Library.CreateLib("ASTAHUB", "Midnight")
 local Tab = Window:NewTab("Tab")
 local Section = Tab:NewSection("Farm")
-Section:NewToggle("ToggleText", "ToggleInfo", function(state)
-    if state then
-        print("Toggle On")
-    else
-        print("Toggle Off")
-    end
-end)
 Section:NewButton("Auto farm level", "ButtonInfo", function()
     print("Clicked")
 end)
